90c8338fdfae5da2aeff302ef77c3debf771f4b9
Martin Gronek <martin@gronek.de>
2024-08-06 20:24:45 +0200

    upstash delete conversation hinzugefügt

---
+++ controllers/deleteConversation.js
@@ -0,0 +1,32 @@
+import fetch from "node-fetch";
+
+export const deleteConversation = async (req, res) => {
+  const { sessionId } = req.body;
+
+  try {
+    const response = await fetch(
+      `https://immune-sawfish-52912.upstash.io/DEL/${sessionId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_API_KEY}`, // Verwende den API-Schlüssel aus der Umgebungsvariable
+        },
+      }
+    );
+
+    if (response.ok) {
+      res.status(200).json({ message: "Konversation erfolgreich gelöscht." });
+    } else {
+      res
+        .status(response.status)
+        .json({ message: "Fehler beim Löschen der Konversation." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Fehler beim Löschen der Konversation.",
+        error: error.message,
+      });
+  }
+};
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotevn.config();
 
 // Controllers
 import { createPrediction } from "./controllers/flowise.js";
+import { deleteConversation } from "./controllers/deleteConversation.js"; // Importiere den neuen Controller
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/api/flowise", createPrediction);
+app.delete("/api/delete-conversation", deleteConversation); // Definiere den neuen Endpunkt
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
